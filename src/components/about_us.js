662178ed2b4a4e590251d76eb380b2607562f95e
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-11-19 16:16:52 +0100

    Implemented lazy image loading

--- src/components/about_us.js
+++ src/components/about_us.js
@@ -1,31 +1,34 @@
 import '../css/about_us.css'
-import image1 from '../img/about_us_img-lazy-3.jpg'
-import image2 from '../img/about_us_img-lazy-2.jpg'
-import image3 from '../img/about_us_img-lazy-1.jpg'
-import image4 from '../img/about_us_img-lazy-1.jpg'
-import image5 from '../img/about_us_img-lazy-1.jpg'
+import image1 from '../img/hotel.jpg'
+import image2 from '../img/about_us_img-2.jpg'
+import image3 from '../img/about_us_img-1.jpeg'
+import image4 from '../img/about_us_img-1.jpeg'
+import image5 from '../img/about_us_img-1.jpeg'
 
 export default function AboutUs() {
+
+
+
     return(
         <div className="about_us--container section" id="about_us">
             <div className="about_us--content">
-                <img src={image1} data-src="https://pix8.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768" alt="photo" className="lazy-img"/>
+                <img src={image1} loading="lazy" alt="photo"/>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laboriosam repudiandae dicta debitis, totam minima assumenda deserunt recusandae necessitatibus voluptatem ullam ad accusantium dolores eos pariatur saepe. Cum libero facere eum, nemo recusandae voluptate ex ad nulla dicta non error ratione, nihil, nesciunt quibusdam architecto odit fugit ipsam omnis obcaecati.</p>      
             </div>        
             <div className="about_us--content reverse">
-                <img src={image2} data-src="https://pix8.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768" alt="photo" className="lazy-img"/>
+                <img src={image2} loading="lazy" alt="photo" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laboriosam repudiandae dicta debitis, totam minima assumenda deserunt recusandae necessitatibus voluptatem ullam ad accusantium dolores eos pariatur saepe. Cum libero facere eum, nemo recusandae voluptate ex ad nulla dicta non error ratione, nihil, nesciunt quibusdam architecto odit fugit ipsam omnis obcaecati.</p>      
             </div>        
             <div className="about_us--content">
-                <img src={image3} data-src="https://pix8.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768" alt="photo" className="lazy-img"/>
+                <img src={image3} loading="lazy" alt="photo" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laboriosam repudiandae dicta debitis, totam minima assumenda deserunt recusandae necessitatibus voluptatem ullam ad accusantium dolores eos pariatur saepe. Cum libero facere eum, nemo recusandae voluptate ex ad nulla dicta non error ratione, nihil, nesciunt quibusdam architecto odit fugit ipsam omnis obcaecati.</p>      
             </div>        
             <div className="about_us--content reverse">
-                <img src={image4} data-src="src/img/hotel.jpg" alt="photo" className="lazy-img"/>
+                <img src={image4} loading="lazy" alt="photo" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laboriosam repudiandae dicta debitis, totam minima assumenda deserunt recusandae necessitatibus voluptatem ullam ad accusantium dolores eos pariatur saepe. Cum libero facere eum, nemo recusandae voluptate ex ad nulla dicta non error ratione, nihil, nesciunt quibusdam architecto odit fugit ipsam omnis obcaecati.</p>      
             </div>        
             <div className="about_us--content">
-                <img src={image5} data-src="src/img/hotel.jpg" alt="photo" className="lazy-img"/>
+                <img src={image5} loading="lazy" alt="photo" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis laboriosam repudiandae dicta debitis, totam minima assumenda deserunt recusandae necessitatibus voluptatem ullam ad accusantium dolores eos pariatur saepe. Cum libero facere eum, nemo recusandae voluptate ex ad nulla dicta non error ratione, nihil, nesciunt quibusdam architecto odit fugit ipsam omnis obcaecati.</p>      
             </div>        
         </div>
