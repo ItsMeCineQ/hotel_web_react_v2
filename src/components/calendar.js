59dc772d5a9552be601a2994c99e6151654523ab
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-11-30 21:19:29 +0100

    Added some css to calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import '../css/calendar.css'
 
 export default function (){
     return(
