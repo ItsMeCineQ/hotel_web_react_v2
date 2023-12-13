b440e58beeb75c20e67da085262fbaa4fc209412
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-12-13 20:41:46 +0100

    Implemented working contact us functionality
    After clicking Send button it checks if all the fields are filled and displays proper message about the state of message.

--- src/components/contact_us.js
+++ src/components/contact_us.js
@@ -1,21 +1,42 @@
+import React, { useState } from 'react';
 import '../css/contact_us.css'
 
 export default function ContactUs() {
+
+    const [name, setName] = useState('');
+    const [mail, setMail] = useState('');
+    const [topic, setTopic] = useState('');
+    const [message, setMessage] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [error, setError] = useState('');
+
+    const sendQuestion = () => {
+        if (!name || !mail || !topic || !message) {
+            setError('Please fill in all fields.');
+        } else {
+            setFormSubmitted(true);
+            setError('');
+        }
+    };
+
+
     return(
         <div className="contact--container section" id="contact">
             <span>Contact us with e-mail!</span>
             <div className="form--contact">
-                <input type="text" placeholder="Your name" />
-                <input type="text" placeholder="Your e-mail" />
-                <select name="Choose topic" className="form--contact-select">
+                <input type="text" placeholder="Your name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type="text" placeholder="Your e-mail" value={mail} onChange={(e) => setMail(e.target.value)}/>
+                <select name="Choose topic" className="form--contact-select" value={topic} onChange={(e) => setTopic(e.target.value)}>
                     <option value="">Choose topic...</option>
                     <option value="Reservation">Reservation</option>
                     <option value="Reservation">Localizations</option>
                     <option value="Reservation">Availability</option>
                     <option value="Reservation">Events</option>
                 </select>
-                <textarea className="form--contact-message" cols="30" rows="10" placeholder="Your message..."></textarea>
-                <button className='button--submit'>Send</button>
+                <textarea className="form--contact-message" cols="30" rows="10" placeholder="Your message..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <button className='button--submit' onClick={sendQuestion}>Send</button>
+                {error && <p className="error-message">{error}</p>}
+                {formSubmitted && !error && <p className="success-message">Your message was sent successfully!</p>}
             </div>
         </div>
     )
