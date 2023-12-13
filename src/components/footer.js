4291406b833f86de4dfc42e212c1d1cb8277f2b1
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-12-13 17:09:04 +0100

    Added logo to the footer
    After clicking logo in the footer the page scrolls to the top

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,16 @@
 import '../css/footer.css'
 
 export default function Footer() {
+
+    const scrollToNav = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
     return(
         <footer>
+            <h2 className="footer--logo" onClick={scrollToNav}>Hotel.ly</h2>
             <p>All rights reserved ©️2023</p>
         </footer>
     )
