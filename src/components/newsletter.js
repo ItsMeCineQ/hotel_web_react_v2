339bb9c239b14968dfe85d9da7065f12c249a652
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2024-01-24 16:49:26 +0100

    Styled newsletter component

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import '../css/newsletter.css'
+import '../css/newsletter.css';
+import icon_mail from '../img/icon_mail.png';
+import icon_close from '../img/icon_close.png';
 
 export default function Newsletter() {
 
@@ -12,8 +14,7 @@ export default function Newsletter() {
         const modal = document.querySelector('.newsletter--success');
         
         if(!email){
-            console.log('blabla')
-            setError('Please fill your email.')
+            setError('Please fill your email.');
         }else{
             setSubmitSuccess(true);
             setEmail('');
@@ -31,13 +32,14 @@ export default function Newsletter() {
 
     return(
         <div className="newsletter hidden">
+            <p>Sign up for the newsletter, receive a discount and stay up to date!</p>
             <input type= 'text' onChange={e => setEmail(e.target.value)} className="newsletter--mail" placeholder="Your email..."></input>
-            <button onClick={openConfirmationModal} className="button_newsletter--submit">Save</button>
-            <button onClick={closeNewsletter} className="button_newsletter--close">x</button>
-            {error && <p>{error}</p>}
+            <button onClick={openConfirmationModal} className="button_newsletter--submit"><img src={icon_mail}></img></button>
+            <button onClick={closeNewsletter} className="button_newsletter--close"><img src={icon_close}></img></button>
+            {error && <p className="newsletter--error">{error}</p>}
             <div className="newsletter--success hidden">
                 <button className="button_newsletter--success--close"></button>
-                {submitSuccess && !error && <h2>You have successfully</h2>}
+                {submitSuccess && !error && <p>You have signed up for newsletter!</p>}
             </div>
         </div>
     )
