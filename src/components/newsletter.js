9ad3abfb2f4d321d4d54df9f03c1923a1f7fdf02
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2024-01-24 20:44:49 +0100

    Optimized the code and added animation
    Newsletter window slides from the right side of the screen now. Aftec closing the window is slides back to the right side.

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -6,7 +6,7 @@ import icon_close from '../img/icon_close.png';
 
 export default function Newsletter() {
 
-    const [email, setEmail] = useState(false);
+    const [email, setEmail] = useState('');
     const [error, setError] = useState(false);
     const [submitSuccess, setSubmitSuccess] = useState(false);
 
@@ -20,20 +20,26 @@ export default function Newsletter() {
             setEmail('');
             setError('');
         }
-
+        
         modal.classList.remove('hidden');
     }
-
+    
     const closeNewsletter = () => {
         const newsletter = document.querySelector('.newsletter');
+        const newsletterSuccess = document.querySelector('.newsletter--success');
 
         newsletter.classList.add('hidden');
+        newsletterSuccess.classList.add('hidden');
+
+        setSubmitSuccess('');
+        setEmail('');
+        setError('');
     }
 
     return(
         <div className="newsletter hidden">
             <p>Sign up for the newsletter, receive a discount and stay up to date!</p>
-            <input type= 'text' onChange={e => setEmail(e.target.value)} className="newsletter--mail" placeholder="Your email..."></input>
+            <input type='text' value={email} onChange={e => setEmail(e.target.value)} className="newsletter--mail" placeholder="Your email..."></input>
             <button onClick={openConfirmationModal} className="button_newsletter--submit"><img src={icon_mail}></img></button>
             <button onClick={closeNewsletter} className="button_newsletter--close"><img src={icon_close}></img></button>
             {error && <p className="newsletter--error">{error}</p>}
