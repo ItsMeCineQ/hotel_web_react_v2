f6c30afa4dd81f5e7da9dedda3651799c9f739e5
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2024-01-29 21:15:29 +0100

    implemented positive modal
    After clicking accept new modal shows up.

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import '../css/newsletter.css';
 import icon_mail from '../img/icon_mail.png';
 import icon_close from '../img/icon_close.png';
+import icon_positive from '../img/icon_positive.png';
 
 export default function Newsletter() {
 
@@ -47,11 +48,13 @@ export default function Newsletter() {
 
     const regulationsAccept = () => {
 
+        const newsletterRegulationsPositive = document.querySelector('.newsletter--confirm--regulations--positive');
         if(!regulationsChecked){
             regulationsDeclined.classList.remove('hidden');
             setRegulationsNotAccepted('Please accept the regulations.');
         } else {
             setRegulationsNotAccepted('');
+            newsletterRegulationsPositive.classList.remove('hidden');
         }
     }
 
@@ -75,39 +78,41 @@ export default function Newsletter() {
     return(
         
         <div className="overlay">
-        <div className='newsletter newsletter--hidden'>
-            <p className="newsletter--message">Sign up for the newsletter, receive a discount and stay up to date!</p>
-            <input type='text' value={email} onChange={e => setEmail(e.target.value)} className="newsletter--mail" placeholder="Your email..."></input>
-            <button onClick={openConfirmationModal} className="button_newsletter--submit"><img src={icon_mail}></img></button>
-            <button onClick={closeNewsletter} className="button_newsletter--close"><img src={icon_close}></img></button>
-            {error && <p className="newsletter--error">{error}</p>}
-            <div className={`newsletter--confirm ${isConfirmationVisible ? '' : 'hidden'}`}>
-                <h2>
-                    Confirm sending the form
-                </h2>
-                <p>
-                    By clicking the "Accept" button you consent to the processing of your personal data
-                    for ther purpose of sending you offers and informations about actual events
-                    and promotions on the provided e-mail address. 
-                </p>
-                <p className="administrator--data">
-                    Data administrator: <br />
-                    Hotel.ly Sp. z o. o. Kraków 12-345, Świętego Tomasza 21
-                </p>
-                <div className="newsletter--confirm--bottom">
-                    <div className="newsletter--confirm--regulations-accept">
-                        <input type="checkbox" className='newsletter--confirm--checkbox' onChange={handleCheckboxChange} checked={regulationsChecked}></input>
-                        <p>Accept the regulations</p>
+            <div className='newsletter newsletter--hidden'>
+                <p className="newsletter--message">Sign up for the newsletter, receive a discount and stay up to date!</p>
+                <input type='text' value={email} onChange={e => setEmail(e.target.value)} className="newsletter--mail" placeholder="Your email..."></input>
+                <button onClick={openConfirmationModal} className="button_newsletter--submit"><img src={icon_mail}></img></button>
+                <button onClick={closeNewsletter} className="button_newsletter--close"><img src={icon_close}></img></button>
+                {error && <p className="newsletter--error">{error}</p>}
+                <div className={`newsletter--confirm ${isConfirmationVisible ? '' : 'hidden'}`}>
+                    <h2>
+                        Confirm sending the form
+                    </h2>
+                    <p>
+                        By clicking the "Accept" button you consent to the processing of your personal data
+                        for ther purpose of sending you offers and informations about actual events
+                        and promotions on the provided e-mail address. 
+                    </p>
+                    <p className="administrator--data">
+                        Data administrator: <br />
+                        Hotel.ly Sp. z o. o. Kraków 12-345, Świętego Tomasza 21
+                    </p>
+                    <div className="newsletter--confirm--bottom">
+                        <div className="newsletter--confirm--regulations-accept">
+                            <input type="checkbox" className='newsletter--confirm--checkbox' onChange={handleCheckboxChange} checked={regulationsChecked}></input>
+                            <p>Accept the regulations</p>
+                        </div>
+                        <p className="newsletter--confirm--regulations--declined">{regulationsNotAccepted}</p>
+                        <div className="buttons_newsletter--confirm">
+                            <button onClick={regulationsDecline} className="button_newsletter--confirm--decline">Decline</button>
+                            <button onClick={regulationsAccept} className="button_newsletter--confirm--accept">Accept</button>
+                        </div>
                     </div>
-                    <p className="newsletter--confirm--regulations--declined">{regulationsNotAccepted}</p>
-                    <div className="buttons_newsletter--confirm">
-                        <button onClick={regulationsDecline} className="button_newsletter--confirm--decline">Decline</button>
-                        <button onClick={regulationsAccept} className="button_newsletter--confirm--accept">Accept</button>
-                    {/* {regulationsAccept ? <p>You have successfully signed up for the newsletter!</p> : ''} */}
+                    <div className="newsletter--confirm--regulations--positive hidden">
+                        <img src={icon_positive}></img>
                     </div>
                 </div>
-             </div>
-        </div>
+            </div>
         </div>
     )
 }
